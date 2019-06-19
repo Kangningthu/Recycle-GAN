96f89e163738cf2233a6872a9f88eacb2f0d9fd5
liukangning <lknthu12@foxmail.com>
2019-06-19 13:51:57 +0200

    fix format

--- scripts/run_Recycle_gan.sh
+++ scripts/run_Recycle_gan.sh
@@ -1,2 +1,8 @@
 #!./scripts/train_recycle.sh
 python train.py --dataroot ./datasets/ --name <NAME> --model recycle_gan  --which_model_netG resnet_6blocks --which_model_netP unet_256 --dataset_mode unaligned_triplet  --no_dropout --gpu 2 --identity 0  --pool_size 0 
+
+
+
+
+
+python train.py --dataroot /srv/glusterfs/liuka/RecycleGAN/Viper_data/Viper/data/recycle-gan --name 0619 --model recycle_gan  --which_model_netG resnet_6blocks --which_model_netP unet_256 --dataset_mode unaligned_triplet  --no_dropout --gpu 2 --identity 0  --pool_size 0
